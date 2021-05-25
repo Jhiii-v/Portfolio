c77138838a98903e4b66ba213c860c8efed47ab2
Jhiii-v <edouard.vieux@me.com>
2021-05-25 10:03:47 +0200

    f

--- src/contents/Contact.js
+++ src/contents/Contact.js
@@ -9,7 +9,7 @@ class Contact extends Component {
             <h1 className="subtopic">Contactez-Moi</h1>
             <div className="hr_line line2 glass"><div className="inner2 inner3"></div></div>
             <div class="con_out glass"><img src={Map} class="co1"></img></div>
-            <a href="#"><div className="back3 glass">Download Resume</div></a>
+            <a href="#"><div className="back3 glass">Téléchargez Mon Cv</div></a>
             <a href="mailto:youremail@gmail.com"><div className="back3 back4 glass">Contact Me</div></a>
             <div className="hr_line line3 glass"><div className="inner3"></div></div>
             <Social />
