c6957649a6ec1d51f71319bf47fc44d552807746
Jhiii-v <edouard.vieux@me.com>
2021-05-25 16:43:25 +0200

    gg

--- src/contents/Portfolio.js
+++ src/contents/Portfolio.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import $ from 'jquery';
 import project1 from '../img/project1.png';
 import project2 from '../img/project2.png';
 import project3 from '../img/project3.png';
