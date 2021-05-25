5643bfcaee409bcb604c515d5ea17e97abbf1b27
Jhiii-v <edouard.vieux@me.com>
2021-05-25 10:24:15 +0200

    Add

--- src/contents/Contact.js
+++ src/contents/Contact.js
@@ -10,7 +10,7 @@ class Contact extends Component {
             <div className="hr_line line2 glass"><div className="inner2 inner3"></div></div>
             <div class="con_out glass"><img src={Map} class="co1"></img></div>
             <a href="#"><div className="back3 glass">Cv    <i class="fas fa-file-download"></i></div></a>
-            <a href="mailto:edouard.vieux@me.com"><div className="back3 back4 glass">Contactez Moi</div></a>
+            <a href="mailto:edouard.vieux@me.com"><div className="back3 back4 glass">Contactez-Moi</div></a>
             <div className="hr_line line3 glass"><div className="inner3"></div></div>
             <Social />
             </div>
