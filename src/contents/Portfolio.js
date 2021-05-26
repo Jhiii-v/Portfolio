4b123bdcb6cdfffc4bb50a3a13c71d911a253b5e
Jhiii-v <edouard.vieux@me.com>
2021-05-26 09:57:24 +0200

    dfb

--- src/contents/Portfolio.js
+++ src/contents/Portfolio.js
@@ -9,29 +9,23 @@ import project6 from '../img/project6.jpg';
 import project7 from '../img/project7.jpg';
 import project8 from '../img/project8.jpg';
 
+const [isActive, setActive] = useState("false");
 
+  const handleToggle = () => {
+    setActive(!isActive);
+  };
 
 
 class Portfolio extends Component {
-    function MyComponent(props) {
-
-      //...
-
-      const mySpecialFunction = () => {
-        console.log('you clicked the button!')
-      }
-
-      const onClickHandler = (e) => {
-        mySpecialFunction();
-      }
 
     render() {
+
 
         return (
             <div className="condiv">
                 <h1 className="subtopic">My Work</h1>
                 <div className="hr_line line2 glass"><div className="inner2 inner3"></div></div>
-                <div id="div1" className="cards">
+                <div id="div1" className={`danger ${isActive ? "hide" : ""}`}>
                     <div className="port_out port1 glass"><a href="https://github.com/vikalp2502/AlgoSort-Sorting-Visualiser"><img src={project1} className="po1"></img></a></div>
                     <div className="port_out port2 glass"><a href="https://github.com/vikalp2502/Shoes-Nike-Landing-page"><img src={project2} className="po1"></img></a></div>
                     <div className="port_out port3 glass"><a href="https://github.com/vikalp2502/To-do-list-v2"><img src={project3} className="po1"></img></a></div>
